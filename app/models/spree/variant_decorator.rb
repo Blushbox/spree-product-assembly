3b25b5457436ed66ecce2b52a191fd1339cc9ce2
Seb Brocher <seb@noctual.com>
2013-06-18 20:52:17 -0500

    experimental (back-end only) support for assembly variants

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,9 +1,50 @@
 Spree::Variant.class_eval do
-  has_and_belongs_to_many  :assemblies, :class_name => "Spree::Product",
+    
+  has_and_belongs_to_many  :assemblies, :class_name => "Spree::Variant",
         :join_table => "spree_assemblies_parts",
         :foreign_key => "part_id", :association_foreign_key => "assembly_id"
 
-  def assemblies_for(products)
-    assemblies.where(id: products)
+  has_and_belongs_to_many  :parts, :class_name => "Spree::Variant",
+        :join_table => "spree_assemblies_parts",
+        :foreign_key => "assembly_id", :association_foreign_key => "part_id"
+
+  def add_part(variant, count = 1)
+   ap = Spree::AssembliesPart.get(self.id, variant.id)
+   if ap
+     ap.count += count
+     ap.save
+   else
+     self.parts << variant
+     set_part_count(variant, count) if count > 1
+   end
+  end
+  
+  def remove_part(variant)
+   ap = Spree::AssembliesPart.get(self.id, variant.id)
+   unless ap.nil?
+     ap.count -= 1
+     if ap.count > 0
+       ap.save
+     else
+       ap.destroy
+     end
+   end
+  end
+  
+  def set_part_count(variant, count)
+   ap = Spree::AssembliesPart.get(self.id, variant.id)
+   unless ap.nil?
+     if count > 0
+       ap.count = count
+       ap.save
+     else
+       ap.destroy
+     end
+   end
+  end
+    
+  def count_of(variant)
+   ap = Spree::AssembliesPart.get(self.id, variant.id)
+   ap ? ap.count : 0
   end
 end
