33b4c38c90dcd2726f2a4890701fd073d70a0b0a
Seb Brocher <seb@noctual.com>
2013-06-25 13:43:18 -0500

    more fixes for assemblies with no variants

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -12,14 +12,14 @@ Spree::Product.class_eval do
   # if product has non-master variants, return those.  Otherwise, return master as only available variant.
   # note this does not return deleted variants (same as regular variants method)  
   def variants_or_master
-    return variants if variants.present?
+    return variants if variants.any?
     [master]
   end
   
   # unlike the original gem whereas assemblies are simpler bundles, here assemblies can have variants, so assembly? truly belongs to a variant instead
   # however, we can say a product is an assembly if at least one of its variants is an assembly
   def assembly?
-    variants.detect(&:assembly?).present?
+    !variants_or_master.detect(&:assembly?).nil?
   end
     
 end
